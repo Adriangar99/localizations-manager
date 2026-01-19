955dc33530c2bd8d569fdfed74a5be04ee6bfaf6
Adrián García <adriangargar99@gmail.com>
2026-01-19 09:39:48 +0000

    Initial Commit

---
+++ LocalizationsManager/LocalizationsManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizationsManagerApp.swift
+//  LocalizationsManager
+//
+//  Created by Adrián García García on 19/1/26.
+//
+
+import SwiftUI
+
+@main
+struct LocalizationsManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LocalizationsManagerTests/LocalizationsManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizationsManagerTests.swift
+//  LocalizationsManagerTests
+//
+//  Created by Adrián García García on 19/1/26.
+//
+
+import Testing
+@testable import LocalizationsManager
+
+struct LocalizationsManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
